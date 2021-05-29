6b607fb459eb7c883579fa8e147d49c5a3036e0c
lagleki <gleki.is.my.name@gmail.com>
2021-05-29 10:36:47 +0300

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "563bbf03549b97e5eaa9"; }
+/******/ 		__webpack_require__.h = function() { return "211cbb18b186e6699c36"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
